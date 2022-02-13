394cdfd8aecc0538a8a5740a26b34f236cb80172
Pieter Pas <pieter.pas@outlook.com>
2022-02-13 15:16:50 +0100

    Package both debug and release in Python wheel Use file-based stubgen

--- QPALM/interfaces/python/qpalm/__init__.py
+++ QPALM/interfaces/python/qpalm/__init__.py
@@ -3,15 +3,12 @@
 __version__ = '1.0.0'
 
 import os
+import typing
 
-try:
-    if os.getenv('QPALM_PYTHON_DEBUG'):
-        from qpalm._qpalmd import *
-        from qpalm._qpalmd import __version__ as c_version
-    else:
-        from qpalm._qpalm import *
-        from qpalm._qpalm import __version__ as c_version
-    assert __version__ == c_version
-except ImportError as e:
-    import warnings
-    warnings.warn("Failed to import the QPALM C++ extension")
+if not typing.TYPE_CHECKING and os.getenv('QPALM_PYTHON_DEBUG'):
+    from qpalm._qpalmd import *
+    from qpalm._qpalmd import __version__ as c_version
+else:
+    from qpalm._qpalm import *
+    from qpalm._qpalm import __version__ as c_version
+assert __version__ == c_version
